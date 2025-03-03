904a9d6b62e2cd7d5daf4c659a5c7e6819c49e18
JossanTorner <josefintorner@gmail.com>
2025-03-03 23:54:59 +0100

    initial commit

---
+++ src/day_3/Day3.kt
@@ -0,0 +1,39 @@
+package day_3
+
+import java.io.File
+
+fun getData() : MutableList<String> {
+    var data = mutableListOf<String>()
+    try{
+        data = File("src/day_3/data_day_3").readLines().toMutableList()
+    }
+    catch (e: Exception){
+        println(e.message)
+    }
+    return data
+}
+
+fun getSum(list : MutableList<String>) : Long {
+    tailrec fun accSum(index : Int, sum : Long) : Long{
+        if (index >= list.size) return sum
+        val currentWord = list[index]
+        val middle = list[index].length / 2
+        val wordDivided = arrayOf(currentWord.substring(0, middle), currentWord.substring(middle))
+        val commonChars = wordDivided[0].toSet().intersect(wordDivided[1].toSet())
+
+        val priorityNum = commonChars.sumOf {
+            when (it) {
+                in 'a'..'z' -> (it.code - 'a'.code + 1).toLong()
+                in 'A'..'Z' -> (it.code - 'A'.code + 27).toLong()
+                else -> 0
+            }
+        }
+        return accSum(index+1, sum + priorityNum)
+    }
+    return accSum(0, 0)
+}
+
+fun main(){
+
+    println(getSum(getData()))
+}
